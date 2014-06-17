a5af90df4e5097d98a28ce606fe2fa846a9bd1ad
Doblera <doblera@students.uni-marburg.de>
2014-06-17 21:46:14 +0200

    Plots und Regression begonnen

--- Doblera/hausaufgabe18.R
+++ Doblera/hausaufgabe18.R
@@ -102,43 +102,48 @@ pyreg <- read.table("Doblera/pyreg.tab",header=TRUE)
 # Wie linreg hat pyreg drei Spalten x1, x2, y
 # Plotten Sie die Punkte + Regressionslinie für y ~ x1 (wie oben).
 
-# CODE_HIER
+ggplot(pyreg,aes(x=x1,y=y)) + geom_point() + geom_smooth(method="lm")
 
 # Und das gleiche für y ~ x2. 
 
-# CODE_HIER
+ggplot(pyreg,aes(x=x2,y=y)) + geom_point() + geom_smooth(method="lm")
 
 # Berechnen Sie die zwei Regressionsmodelle für y ~ x1 und y ~ x2
 
-# CODE_HIER
+lm.x1.pyreg<-lm(y ~ x1, data=pyreg)
+summary(lm.x1.pyreg)
 
-# CODE_HIER
+lm.x2.pyreg<-lm(y~ x2, data=pyreg)
+summary(lm.x2.pyreg)
 
 # Bevor Sie die Regression y ~ x1 + x2 berechnen, schauen Sie sich die
 # Korrelation (mit Konfidenzintervall!) zwischen x1 und x2 an:
 
-# CODE_HIER
+cor(pyreg$x1,pyreg$x2)
 
 # Wenn Sie nicht miteinander signifikant korreliert sind, sollten Sie auch die
 # Regression y ~ x1 + x2 berechnen:
 
-# CODE_HIER
+lm.3.pyreg<- lm(y ~ x1 + x2, data=pyreg)
+summary(lm.3.pyreg)
 
 # Wie gut passt das lineare Modell zu den Daten? Schauen Sie sich die R^2 und 
 # F-Werte an sowie auch die t-Werte für die einzelnen Prediktoren. Glauben Sie, 
 # dass y im linearen Verhältnis zu x1 und x2 steht? Machen Sie eine Grafik wie
 # oben für y ~ x1 + x2, **nachdem Sie sich eine Antwort überlegt haben**.
 
-# CODE_HIER
+ggplot(linreg,aes(x=x1,y=x2)) + geom_point(aes(size=y))
 
 # Glauben Sie jetzt, dass y im linearen Verhältnis zu x1 und x2 steht? Warum (nicht)?
 
 # Wie sieht mit Korrelationen aus? Berechnen Sie die Korrelation (sowohl Pearson
 # als auch Spearman) zwischen (y und x1) sowie auch zwischen (y und x2). 
 
-# CODE_HIER
+cor(pyreg$y,pyreg$x1, method="pearson")
+cor(pyreg$y,pyreg$x1, method="spearman")
 
-# CODE_HIER 
+cor(pyreg$y,pyreg$x2, method="pearson")
+cor(pyreg$y,pyreg$x2, method="spearman")
 
 # Welche Art von Korrelation macht am meisten Sinn bei diesen Daten?
 
